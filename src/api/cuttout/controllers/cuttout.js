a2be3f06ac49a214d0a7c413f75bf85612e0d4db
Tony Grimes <acidtone@tonygrimes.com>
2022-11-14 13:42:32 -0700

    Initial commit

---
+++ src/api/cuttout/controllers/cuttout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cuttout controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cuttout.cuttout');
---
+++ src/api/cuttout/routes/cuttout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cuttout router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cuttout.cuttout');
